9e8f585bc9614837ac5a40c179704d25cc11a59b
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-15 14:14:23 +0200

    server writing down pictures

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,14 +5,10 @@ exports.upload = function(request, response) {
     console.log("Beginning upload request service.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-      // fs.copyFile(files.upload.path, 'F:/Adam/programowanie/Kodilla/node.js-13.9', function(err) {
-      //   if (err) throw err;
-      //   console.log(files.upload.path + ' was copied to Kodilla/node.js-13.9');
-      // });
-      form.on('fileBegin', function(name, file) {
+        form.on('fileBegin', function(name, file) {
         file.path('F:/Adam/programowanie/Kodilla/node.js-13.9')
       });
-      fs.renameSync(files.upload.path, "test.png");
+      fs.renameSync(files.upload.path, 'test.png');
       response.writeHead(200, {"Content-Type": "text/html"});
       response.write("received image:<br>");
       response.write("<img src='/show'>");
